34ce00403f99f06049eefc723cc9785e2a20c7ef
Yuki Inoue <inouetakahiroki@gmail.com>
2016-10-06 09:43:25 +0900

    Add docstring to tblui
    As requested by @syohex at https://github.com/melpa/melpa/pull/4298.

--- tblui.el
+++ tblui.el
@@ -44,6 +44,36 @@
 ;;;###autoload
 (defmacro tblui-define (tblui-name entries-provider table-layout popup-definitions)
 
+  "Define tabulated list UI easily.  Hereafter referred as tblui.
+This macro defines functions and popups for the defined tblui.
+User of this macro can focus on writing the logic for ui, let this
+package handle the tabulated list buffer interaction part.
+
+Each arguments are explained as follows:
+
+ * `TBLUI-NAME` : the symbol name of defining tblui.  It will be used
+                  as prefix for functions defined via this macro.
+ * `ENTRIES-PROVIDER` : the function which provides tabulated-list-entries
+ * `TABLE-LAYOUT` : the `tabulated-list-format` to be used for the tblui.
+ * `POPUP-DEFINITIONS` : list of popup definition.
+   A popup definition is an assoc of
+       `((:key . KEY) (:name . NAME) (:funcs . FUNCTIONS))`.
+   KEY is the key to be bound for the defined magit-popup.
+   NAME is the name for defined magit-popup.
+   FUNCTIONS is the list of action definition.
+   Action definition is a list of 3 elements,
+   which is `(ACTIONKEY DESCRIPTION FUNCTION)`.
+
+   ACTIONKEY is the key to be used as action key in the magit-popup.
+   DESCRIPTION is the description of the action.
+   FUNCTION is the logic to be called for this UI.
+   It is the elisp function which receives the ID of tabulated-list entry,
+    and do what ever operation.
+
+With this macro `TBLUI-NAME-goto-ui` function is defined.
+Calling this function will popup and switch to the tblui buffer."
+
+
   (let* ((goto-ui-symbol
           (tblui--append-str-to-symbol tblui-name "-goto-ui"))
          (ui-buffer-name
